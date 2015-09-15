f253a1a6d2a0234c57c266d6147aba48b7419af9
Lokaler Benutzer <Lokaler Benutzer@P-143-07.Schule.107842.mts-schule.de>
2015-09-15 05:09:43 -0300

    Init Filesystem

---
+++ src/madn/controller/FigurenController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package madn.controller;
+
+/**
+ *
+ * @author Lokaler Benutzer
+ */
+public class FigurenController {
+    
+}
---
+++ src/madn/model/Spielfeld.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package madn.model;
+
+/**
+ *
+ * @author Lokaler Benutzer
+ */
+public class Spielfeld {
+    
+}
