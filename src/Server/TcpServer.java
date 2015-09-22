d9942d5f38eadd9357d2ab4bb99700c22d5ad3b7
Lokaler Benutzer <Lokaler Benutzer@P-143-07.Schule.107842.mts-schule.de>
2015-09-22 05:20:22 -0300

    Anpassungen Server

--- src/Server/TcpServer.java
+++ src/Server/TcpServer.java
@@ -13,17 +13,28 @@ import java.io.*;
 import java.net.*; 
 public class TcpServer 
 {    
+    /**
+     *
+     * @param argv
+     * @throws Exception
+     */
     public static void main(String argv[]) throws Exception       
     {          
         String clientSentence;          
         String capitalizedSentence;          
-        ServerSocket welcomeSocket = new ServerSocket(6789);          
+        ServerSocket welcomeSocket = new ServerSocket(80); 
+        System.out.println("Starte Listener");  
         while(true)          
         {             
-            Socket connectionSocket = welcomeSocket.accept();             
+            System.out.println("Warten auf Client");  
+            Socket connectionSocket = welcomeSocket.accept();   
+            System.out.println("Client Connected");  
             BufferedReader inFromClient =                
                     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));             
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());             
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());  
+            System.out.println("Sending to Client");  
+            outToClient.writeChars("Fu Fu Fa Fo" + '\n');  
+            System.out.println("Waiting for Response");  
             clientSentence = inFromClient.readLine();             
             System.out.println("Received: " + clientSentence);             
             capitalizedSentence = clientSentence.toUpperCase() + '\n';             
