c4192f80bb0ba985f65a6ae16656074190502fad
mfrye <mfrye@NMSNB260.noventum.int>
2015-09-22 09:42:12 +0200

    Client / Server...

--- src/Client/TcpClient.java
+++ src/Client/TcpClient.java
@@ -9,9 +9,21 @@ package Client;
  *
  * @author mfrye
  */
-public class TcpClient {
-        public static void main(String[] args) {
-        // TODO code application logic here
-            
-    }
+import java.io.*; 
+import java.net.*; 
+public class TcpClient
+{  
+    public static void main(String argv[]) throws Exception  
+    {   
+        String sentence;   
+        String modifiedSentence;   
+        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));   
+        Socket clientSocket = new Socket("google.de", 80);  
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   
+        sentence = inFromUser.readLine();   outToServer.writeBytes(sentence + '\n');   
+        modifiedSentence = inFromServer.readLine();   
+        System.out.println("FROM SERVER: " + modifiedSentence);   
+        clientSocket.close();  
+    } 
 }
