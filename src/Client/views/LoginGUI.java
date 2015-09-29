a4672eef020c4330c3e012e9e10acddd2ed10957
Lokaler Benutzer <Lokaler Benutzer@P-143-07.Schule.107842.mts-schule.de>
2015-09-29 05:51:27 -0300

    Fertige Login-View ActionListener Implementiert für Schnittstelle zum gameController

--- src/Client/views/LoginGUI.java
+++ src/Client/views/LoginGUI.java
@@ -4,10 +4,50 @@
  */
 package Client.views;
 
+import java.awt.Canvas;
+import java.awt.Frame;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 /**
  *
  * @author Lokaler Benutzer
  */
-public class LoginGUI {
+class LoginGUI extends Frame{
     
+    private Frame login;
+    private TextField loginField;
+    private Button loginButton = new Button("Einloggen");
+    
+    LoginGUI(){
+        super("Login");
+        
+        initForm();
+    }
+    
+    private void initForm(){
+        loginField = new TextField();
+        this.add(loginField);
+        this.add(loginButton);
+    }
+    
+    public String getName(){
+        return this.loginField.getText();
+    }
+    
+    public void setLoginListener(ActionListener l){
+        this.loginButton.addActionListener(l);
+    }
+    
+    public static class CloseListener extends WindowAdapter
+	{	public void windowClosing(WindowEvent e)
+		{	e.getWindow().setVisible(false);
+			System.exit(0);
+		}
+	}
 }
